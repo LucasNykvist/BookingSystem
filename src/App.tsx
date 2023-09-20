1d5d4532c1b0b0ad9e59892ec876243ba42aacb2
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-09-20 19:10:08 +0200

    Removed text

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <p>Fuckssssssss!</p>
-      <p>Fucker</p>
+
     </>
 
   );
---
+++ src/views/Homeview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homeview: React.FC = () => {
+    return (
+        <div>Homeview</div>
+    )
+}
+
+export default Homeview
