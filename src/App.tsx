6e89879de188efa58682155f419d1e4dc0bbbfb1
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-09-20 19:44:09 +0200

    started styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homeview from './views/Homeview';
 
 function App() {
   return (
     <>
-
+      <Homeview></Homeview>
     </>
 
   );
--- src/views/Homeview.tsx
+++ src/views/Homeview.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Homeview: React.FC = () => {
     return (
-        <div>Homeview</div>
+        <>
+            <Navbar></Navbar>
+        </>
     )
 }
 
