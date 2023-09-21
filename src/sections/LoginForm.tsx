982f45f226ca0e91057016185d8633243ef5e1b3
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-09-21 18:54:43 +0200

    work in progress on Login and Register frontend

---
+++ src/sections/LoginForm.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginForm = () => {
+    return (
+        <div className="container mt-5">
+            <form className='loginForm' action="post">
+                <h2>BS | BOOKING SYSTEM | LOGIN</h2>
+                <input className='inputEmail' type="text" placeholder='Enter your Email' />
+                <input className='inputPassword' type="text" placeholder='Enter your Password' />
+                <button className='loginButton'>LOGIN</button>
+                <p>Don't Have An Account? <Link className='registerLink' to='/register'>Click Here To Register!</Link></p>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import LoginForm from '../sections/LoginForm'
 
 const LoginView = () => {
     return (
         <>
             <Navbar></Navbar>
+            <LoginForm></LoginForm>
         </>
     )
 }
