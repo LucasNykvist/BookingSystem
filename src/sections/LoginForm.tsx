631fa53100b5b219490e82159cc7c0f3f76459d2
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-09-21 19:10:59 +0200

    Ting

--- src/sections/LoginForm.tsx
+++ src/sections/LoginForm.tsx
@@ -6,9 +6,12 @@ const LoginForm = () => {
         <div className="container mt-5">
             <form className='loginForm' action="post">
                 <h2>BS | BOOKING SYSTEM | LOGIN</h2>
+
                 <input className='inputEmail' type="text" placeholder='Enter your Email' />
                 <input className='inputPassword' type="password" placeholder='Enter your Password' />
+
                 <button className='loginButton'>LOGIN</button>
+
                 <p>Don't Have An Account? <Link className='registerLink' to='/register'>Click Here To Register!</Link></p>
             </form>
         </div>
--- src/sections/RegisterForm.tsx
+++ src/sections/RegisterForm.tsx
@@ -7,11 +7,14 @@ const RegisterForm = () => {
         <div className="container mt-5">
             <form className='loginForm' action="post">
                 <h2>BS | BOOKING SYSTEM | REGISTER</h2>
+
                 <input className='inputEmail' type="email" placeholder='Enter your Email' />
                 <input className='inputEmail' type="text" placeholder='Enter your Company Name' />
                 <input className='inputEmail' type="tel" placeholder='Enter your Phone Number' />
                 <input className='inputPassword' type="password" placeholder='Enter your Password' />
+
                 <button className='loginButton'>REGISTER</button>
+
                 <p>Already Have An Account? <Link className='registerLink' to='/login'>Click Here To Login!</Link></p>
             </form>
         </div>
