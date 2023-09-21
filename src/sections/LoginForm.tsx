c220c19dbf817a4f7b2ccee0ffa674b42fdcb02f
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-09-21 19:06:20 +0200

    added register frontend

--- src/sections/LoginForm.tsx
+++ src/sections/LoginForm.tsx
@@ -7,7 +7,7 @@ const LoginForm = () => {
             <form className='loginForm' action="post">
                 <h2>BS | BOOKING SYSTEM | LOGIN</h2>
                 <input className='inputEmail' type="text" placeholder='Enter your Email' />
-                <input className='inputPassword' type="text" placeholder='Enter your Password' />
+                <input className='inputPassword' type="password" placeholder='Enter your Password' />
                 <button className='loginButton'>LOGIN</button>
                 <p>Don't Have An Account? <Link className='registerLink' to='/register'>Click Here To Register!</Link></p>
             </form>
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import RegisterForm from '../sections/RegisterForm'
 
 const RegisterView = () => {
     return (
         <>
             <Navbar></Navbar>
+            <RegisterForm></RegisterForm>
         </>
     )
 }
