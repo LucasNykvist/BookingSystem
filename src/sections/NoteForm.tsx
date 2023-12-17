b126bf67193bb13b83f96b703e0d577f21802814
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-12-17 10:26:37 +0100

    Frontend shi

--- src/sections/NoteForm.tsx
+++ src/sections/NoteForm.tsx
@@ -4,13 +4,19 @@ import React from "react";
 const NoteForm = () => {
   return (
     <div className="note-form-container">
-      <form action="">
+      <form method="post">
         <div className="input-fields">
           <h1>CREATE NOTE</h1>
-          <TextField label="Author"></TextField>
-          <TextField label="Title"></TextField>
-          <TextField maxRows={4} multiline label="Content"></TextField>
-          <Button color="secondary" variant="contained">
+          <TextField required label="Title"></TextField>
+          <TextField required label="Author"></TextField>
+          <TextField
+            className="content-field"
+            required
+            rows={4}
+            multiline
+            label="Content"
+          ></TextField>
+          <Button type="submit" color="secondary" variant="contained">
             Create Note
           </Button>
         </div>
