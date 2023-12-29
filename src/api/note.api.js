1b02ae129687b3c88a057ef75ef6b2b2c3c3e87d
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-12-29 18:03:30 +0100

    Frontend Progress

--- src/api/note.api.js
+++ src/api/note.api.js
@@ -11,3 +11,21 @@ export const createNote = async (note) => {
     console.error(error); // Handle any errors that occur during the request
   }
 };
+
+export const getAllNotes = async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/api/note");
+    return response.data;
+  } catch (error) {
+    console.error(error); // Handle any errors that occur during the request
+  }
+};
+
+export const deleteNote = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:5000/api/note/${id}`);
+    console.log(response.data); // Handle the response data here
+  } catch (error) {
+    console.error(error); // Handle any errors that occur during the request
+  }
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BookerLogo from "../assets/images/BookerLogo.png";
 import { NavLink } from "react-router-dom";
+import { Button, ButtonGroup } from "@mui/material";
 
 const Navbar = () => {
   return (
@@ -8,7 +9,26 @@ const Navbar = () => {
       <div className="container-fluid">
         <div className="navbar-brand">TDL | NOTE APPLICATION</div>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav"></div>
+          <div className="navbar-nav">
+            <ButtonGroup>
+              <Button
+                href="/"
+                sx={{ backgroundColor: "black", fontSize: "1rem" }}
+                variant="contained"
+                color="secondary"
+              >
+                Create Note
+              </Button>
+              <Button
+                href="/notes"
+                sx={{ backgroundColor: "black", fontSize: "1rem" }}
+                variant="contained"
+                color="secondary"
+              >
+                Manage Notes
+              </Button>
+            </ButtonGroup>
+          </div>
         </div>
       </div>
     </nav>
