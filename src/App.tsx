30a006b4a78998231a13c37425e37b8454d5240f
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-03 22:11:24 +0100

    Update Nav.scss and Navbar.tsx, add LoginForm and NotFound components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Homeview from "./views/Homeview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ManageNotes from "./views/ManageNotes";
 import RegisterForm from "./pages/User/RegisterForm";
+import LoginForm from "./pages/User/LoginForm";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Homeview />} />
         <Route path="/notes" element={<ManageNotes />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
