d4e16708d4af64321c886100db5e3e9f8669497b
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-05 22:37:21 +0100

    Update Navbar and HomePage components

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import { Button } from "@mui/material";
 
 const HomePage = () => {
   return (
@@ -18,7 +19,7 @@ const HomePage = () => {
           style={{
             backdropFilter: "blur(20px)",
             backgroundColor: "rgba(255, 255, 255, 0.5)",
-            width: "700px",
+            maxWidth: "700px",
             padding: "1rem",
             borderRadius: "1rem",
             display: "flex",
@@ -42,14 +43,40 @@ const HomePage = () => {
           <h1
             style={{
               fontWeight: "bold",
-              textAlign: "center",
               marginTop: "1rem",
+              color: "black",
             }}
           >
             WELCOME!
           </h1>
 
-          <div className="option-list"></div>
+          <div
+            style={{
+              display: "flex",
+              gap: "0.5rem",
+              marginTop: "1rem",
+              flexDirection: "column",
+              color: "black",
+              fontSize: "1.1rem",
+            }}
+            className="option-list"
+          >
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+              veritatis ipsa autem possimus! Consequuntur libero tenetur, quia
+              omnis animi facilis veniam quo optio beatae eaque officiis, sequi
+              nesciunt mollitia expedita!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              expedita veniam eum nemo laboriosam, asperiores illo atque
+              veritatis facere repellendus culpa eaque officia dolorum
+              praesentium dignissimos, corrupti nulla, libero porro.
+            </p>
+
+            <img src="https://media.tenor.com/E0idbj67jFIAAAAd/fortnite-fortnite-default-dance.gif" />
+          </div>
         </div>
       </div>
     </>
