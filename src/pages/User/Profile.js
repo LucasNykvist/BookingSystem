6842b0298b91e8a915ec410cf5433e94d2a88a70
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-06 01:24:11 +0100

    Add user information section to profile page

--- src/pages/User/Profile.js
+++ src/pages/User/Profile.js
@@ -27,6 +27,15 @@ const Profile = () => {
           <h1>{loggedInUser.replace(/"/g, "")}</h1>
 
           <div className="profile-information">
+            <h4 style={{ color: "black", fontWeight: "bold" }}>
+              User Information:{" "}
+            </h4>
+            <p className="profile-info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
+              delectus? Earum suscipit illo, omnis voluptas, laudantium eligendi
+              et itaque, ipsam quaerat dolore aliquam voluptate voluptatibus
+              accusamus fuga beatae non est?
+            </p>
             <p className="profile-info-text">Notes Created: 3</p>
             <p className="profile-info-text">Registered: 2024-01-04</p>
           </div>
