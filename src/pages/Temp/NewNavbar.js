96dd0b8c2e2108540a6113aa49c1eb2b5602993a
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-11 18:36:24 +0100

    Update dependencies and add mobile menu icon

--- src/pages/Temp/NewNavbar.js
+++ src/pages/Temp/NewNavbar.js
@@ -1,14 +1,29 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  Icon,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NewNavbar = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="x1">
         <Toolbar disableGutters>
-          <Typography variant="h6" noWrap>
+          <Typography className="logo" variant="h6" noWrap>
             TDL
           </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton size="large" className="menuButton">
+              <MenuIcon />
+            </IconButton>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
